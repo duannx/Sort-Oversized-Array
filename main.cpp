95d9f5aca39f08e4dcb35ec4e16bf32ab99a1d41
Duannx <xuanduannguyen@gmail.com>
2016-05-24 00:53:16 +0700

    Add input.txt (10000 integer) and read input print result to text file

--- main.cpp
+++ main.cpp
@@ -1,22 +1,69 @@
 #include <iostream>
-#include <algorithm>
+#include <algorithm>  	
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <fstream>
+#include <string> 
+#include <sstream>
 
-using namespace std;
-
-const int SIZE = 8;
+using namespace std; 
 
 int main(){
-   int intArray[SIZE] = {5, 3, 32, -1, 1, 104, 53,54};
-
-    //call sort function
-    sort(intArray, intArray + SIZE);
-
-	//show sorted array
-    cout << "Sorted Array: " << endl;
-    for (size_t i = 0; i != SIZE; ++i)
-        cout << intArray[i] << " ";
-
-   return 0;
+	const int size = 2500; 	//size of an vector
+	int pos = 1;			// indices of vector
+    int c;					// store char
+	int i=0;				// use for for loop
+	int index = 1;			// number of vector
+    string str ="";			// store each number
+    vector <int> arr; 		// store numbers
+	
+	// Read file input and write sorted array to other file
+    FILE *file = fopen("input.txt", "r"); //Open file
+    if(file) {
+      do {  
+          c = fgetc(file);			//Read a char
+          if(c != EOF) {
+          	if(c == 32 && str!=""){ //End a number (if char = ' ')
+         		arr.push_back(atoi( str.c_str() )); //Convert to int and add to vector
+				str = "";			//Set str empty to start new number
+				pos++;				//increase indice
+			}
+			if(c != 32){			// if char != ' '
+			  	str.push_back((char) c); //Add char to str
+			}
+		  }  
+		  
+          if(pos > size || c == EOF) { //If vector have enoungh element. Sort vector. Write to part file and coninue while loop
+            sort(arr.begin(),arr.end()); // Sort vector
+            
+            //Create file name
+            string file_name;
+		    ostringstream oss;
+			oss << "part" << index << ".txt";
+			file_name = oss.str();  
+			
+			//Write file
+			ofstream outfile;
+			outfile.open (file_name.c_str(),ios::out | ios::app | ios::binary);
+			for(i=0;i<arr.size();i++){
+		    	outfile << arr[i] << " ";
+			}
+			outfile << "\n";
+			outfile.close();
+			
+			index ++;
+			arr.clear();
+			str =  "";
+			pos = 0;  
+          }           
+      } while(c != EOF); 
+      fclose(file);           
+    }
+     
+    
+ 	 
+    return 0;
 }
 
 
